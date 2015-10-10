8d61b46293496a95fe5cedaccb0b606470452a4e
Hans <hans.eschbaum@gmail.com>
2015-10-11 02:20:09 +0300

    Picture description forms

--- application/views/upload_form.php
+++ application/views/upload_form.php
@@ -8,6 +8,10 @@
 
 <?php echo form_open_multipart('upload/do_upload');?>
 
+Title: <input type="text" name="title">
+Description: <input type="text" name="description">
+Location: <input type="text" name="location">
+
 <input type="file" name="userfile" size="20" />
 
 <br /><br />
