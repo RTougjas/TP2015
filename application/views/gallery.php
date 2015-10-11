6cea0c9240fed92e1b9d0c5028b1eda748737c19
Hans <hans.eschbaum@gmail.com>
2015-10-11 18:32:44 +0300

    no message

--- application/views/gallery.php
+++ application/views/gallery.php
@@ -10,11 +10,13 @@
      <tr>
       <td><strong>Picture Title</strong></td>
       <td><strong>Picture description</strong></td>
+	  <td><strong>Picture</strong></td>
     </tr> 
      <?php foreach($pictures as $picture){?>
      <tr>
          <td><?php echo $picture->title;?></td>
          <td><?php echo $picture->description;?></td>
+		 <td><?php echo $picture->location;?></td>
       </tr>     
      <?php }?>  
    </table>
