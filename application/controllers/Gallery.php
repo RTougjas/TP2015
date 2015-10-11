b1fdc3306cb7035688659f1f7cd803a150659d20
Hans <hans.eschbaum@gmail.com>
2015-10-11 17:55:26 +0300

    tests

--- application/controllers/Gallery.php
+++ application/controllers/Gallery.php
@@ -3,14 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Gallery extends CI_Controller {
 	public function __construct()
-        {
+	{
          parent::__construct();
 	}
 	public function index()
 	{
 		$this->load->database();  
 		
-		$data = $this->db->query('SELECT title, description, location FROM pictures');
+		$data = array($this->db->query('SELECT title, description, location FROM pictures'));
 
 		$this->load->view('gallery', $data);
 		
--- application/views/gallery.php
+++ application/views/gallery.php
@@ -3,7 +3,7 @@ Siia tuleb siis basic galerii alguses.
 <img src="/uploads/Swedistan.png" alt="Swedistan"> 
 
 <ul>
-<?php foreach ($upload_data as $item => $value):?>
+<?php foreach ($data as $item => $value):?>
 <li><?php echo $item;?>: <?php echo $value;?></li>
 <?php endforeach; ?>
 </ul>
