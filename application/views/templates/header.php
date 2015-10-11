3ceae2ee71b31807397209bac0be4cf00724729f
Kaarel <kaarel06@gmail.com>
2015-10-11 19:20:16 +0300

    changed links in navbar

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -12,17 +12,17 @@
         <div class="navbar">
             <div class="navbar-inner">
                 <ul class="nav">
-                    <li><a href=<?php echo base_url("gallery"); ?>>Gallery</a></li>
-                    <li><a href=<?php echo base_url("upload"); ?>>Upload</a></li>
+                    <li><a href=<?php echo base_url("index.php/gallery"); ?>>Gallery</a></li>
+                    <li><a href=<?php echo base_url("index.php/upload"); ?>>Upload</a></li>
                     <?php
                         if($this->session->has_userdata('username')){
-                            echo "<li><a href=".base_url('profile/'.$this->session->userdata('username')).">"
+                            echo "<li><a href=".base_url('index.php/profile/'.$this->session->userdata('username')).">"
                                 .$this->session->userdata('username')
                                 ."</a></li>";
-                            echo "<li><a href=".base_url('logout').">Log out</a></li>";
+                            echo "<li><a href=".base_url('index.php/logout').">Log out</a></li>";
                         }else{
-                            echo "<li><a href=".base_url('Login').">Login</a></li>";
-                            echo "<li><a href=".base_url('registration').">Registration</a></li>";
+                            echo "<li><a href=".base_url('index.php/Login').">Login</a></li>";
+                            echo "<li><a href=".base_url('index.php/registration').">Registration</a></li>";
                         }
                     ?>
                 </ul>
