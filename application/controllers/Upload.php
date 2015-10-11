654330d52368603616194d73c3cf19af1b3dc396
Hans <hans.eschbaum@gmail.com>
2015-10-11 20:14:16 +0300

    no message

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -46,13 +46,15 @@ class Upload extends CI_Controller {
 						$this->db->insert('pictures', $info);
 						
 						$this->load->library('ftp');
-						$config['hostname'] = 'ftp://steffi.ee';
+						$config['hostname'] = 'ftp.steffi.ee';
 						$config['username'] = getenv('FTP_USER');
 						$config['password'] = getenv('FTP_PASSWORD');
 						$config['debug']	= TRUE;
 						
 						$this->ftp->connect($config);
 						$this->ftp->upload('/uploads/Swedistan.png', '/fotod/Swedistan.png', 'auto', 0775);
+						$this->ftp->close();
+
 						
 						$this->load->view('templates/header');
                         $this->load->view('upload_success', $data);
