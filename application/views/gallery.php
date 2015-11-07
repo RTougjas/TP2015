8e6cc2ead73c2542dcc2e4ba955841a3e99eea71
Kaarel <kaarel06@gmail.com>
2015-11-07 15:42:20 +0200

    Resolved merge conflicts

--- application/views/gallery.php
+++ application/views/gallery.php
@@ -10,11 +10,7 @@
      <tr>
          <td><?php echo $picture->title;?></td>
          <td><?php echo $picture->description;?></td>
-<<<<<<< HEAD
-		 <td><?php echo '<a href='.site_url('picture/photo/'.$picture->id).'>
-=======
-		 <td><?php echo '<a href="picture/'.$picture->id.'">
->>>>>>> eeab54fccca98b1666c67bd49d12c1350c06465d
+		 <td><?php echo '<a href='.site_url("picture/".$picture->id).'>
 		 <img src="'.$picture->location.'" alt="borked" height="42" width="42">';?>
 		 </a></td>
       </tr>     
