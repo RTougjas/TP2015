e08bea713a28e603c0565963c0226822435e655a
Kaarel <kaarel06@gmail.com>
2015-11-09 04:14:06 +0200

    fixed uploading

--- application/views/upload_form.php
+++ application/views/upload_form.php
@@ -1,7 +1,7 @@
 
 
 
-
+<?php echo form_open_multipart('upload/do_upload');?>
 <div class = "form-container" >
     Title: <input type="text" name="title">
     Description: <input type="text" name="description">
@@ -9,7 +9,7 @@
     <input type="file" name="userfile" size="20" />
 <div class = "error_message">
     <?php echo $error;?>
-    <?php echo form_open_multipart('upload/do_upload');?>
+    
     </div>
 
     <input type="submit" value="upload" />
