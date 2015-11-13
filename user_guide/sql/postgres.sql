05350cf28a98353fd1cd01b646cb9cff35c0bdca
Kaarel <kaarel06@gmail.com>
2015-11-13 06:41:08 +0200

    split database file into 2

--- user_guide/sql/postgres.sql
+++ user_guide/sql/postgres.sql
@@ -43,17 +43,6 @@ CREATE TABLE IF NOT EXISTS "users_groups" (
 );
 
 
-INSERT INTO groups (id, name, description) VALUES
-    (1,'admin','Administrator'),
-    (2,'members','General User');
-
-INSERT INTO users (ip_address, username, password, salt, email, activation_code, forgotten_password_code, created_on, last_login, active, first_name, last_name, company, phone) VALUES
-    ('127.0.0.1','administrator','$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36','','admin@admin.com','',NULL,'1268889823','1268889823','1','Admin','istrator','ADMIN','0');
-
-INSERT INTO users_groups (user_id, group_id) VALUES
-    (1,1),
-    (1,2);
-
 CREATE TABLE IF NOT EXISTS "login_attempts" (
     "id" SERIAL NOT NULL,
     "ip_address" varchar(15),
@@ -63,7 +52,7 @@ CREATE TABLE IF NOT EXISTS "login_attempts" (
   CONSTRAINT "check_id" CHECK(id >= 0)
 );
 
-CREATE TABLE "ci_sessions" (
+CREATE TABLE IF NOT EXISTS "ci_sessions" (
         "id" varchar(40) NOT NULL,
         "ip_address" varchar(45) NOT NULL,
         "timestamp" bigint DEFAULT 0 NOT NULL,
