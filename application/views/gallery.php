4623f7b1b82a906a13e4954f9e8b2206e6fb7fd1
RTougjas <raintoug@ut.ee>
2015-11-18 21:34:14 +0200

    enhanced gallery view

--- application/views/gallery.php
+++ application/views/gallery.php
@@ -1,18 +1,29 @@
+<div class="container">
 <?php for($i = 0; $i < sizeOf($pictures); $i++) {?>
-	<div class="col-lg-2 col-md-4 col-sm-4">
+	<div class="col-lg-4 col-md-4 col-sm-4">
 			<table>
 				<tr>
 					<th><?php echo $pictures[$i]->title?><th>
 				</tr>
 				<tr>
-					<td><?php echo $pictures[$i]->description?><td>
+					<td><?php
+						$description = $pictures[$i]->description;
+						
+						if(strlen($description) < 25) {
+							echo $description;
+						}else {
+							echo substr($description, 0, 25).'...';
+						}?>
+					<td>
 				</tr>
 				<tr>
 					<td><?php echo '<a href='.site_url("picture/".$pictures[$i]->id).'>
-						 <img src="'.$pictures[$i]->location.'" alt="borked" height="200" width="200">';?></td>
+						 <img src="'.$pictures[$i]->location.'" class="img-rounded" alt="borked" height="200" width="200">';?></td>
 				</tr>
 			</table>
+			<br>
 		</div>
 <?php } ?>
+</div>
 
 
