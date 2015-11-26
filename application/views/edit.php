21564f0ab7023d37a6f8b39701f92c2b27a15be5
RTougjas <raintoug@ut.ee>
2015-11-26 03:58:44 +0200

    debugging
    removed left side of panel body

--- application/views/edit.php
+++ application/views/edit.php
@@ -8,7 +8,47 @@
 				<input type="text" class="form-control" name="title" placeholder="<?php echo $picture->title;?>">
   			</div>
 		</div>
-
+  	  	<div class="panel-body">
+			<div class="row">
+				
+				<div class="col-lg-6 col-md-6 col-xs-6">
+					<?php if($owner) {?>
+						<table class="table">
+							<tr>
+								<th><h3>Asub Sinu albumites</h3></th>
+							</tr>
+							<?php for($i = 0; $i < sizeOf($albums); $i++) {?>
+								<tr>
+									<td>
+										<?php echo $albums[$i]->title;?>
+									</td>
+									<?php if(sizeOf($photo_in_albums) > 0) {?> 
+										<?php for($j = 0; $j < sizeOf($photo_in_albums); $j++) { ?>
+											<?php if($photo_in_albums[$j]->album_id == $albums[$i]->id) { ?>
+												<td>
+													<?php echo '<input type="checkbox" name="albums[]" value="'.$albums[$i]->id.'" checked>';?>
+												</td>
+												</tr>
+												<?php break?>
+											<?php } ?>
+											<!-- for cases when there will be no break out of this loop. -->
+											<?php if($j == sizeOf($photo_in_albums) - 1) {?>
+												<td>
+													<?php echo '<input type="checkbox" name="albums[]" value="'.$albums[$i]->id.'">';?>
+												</td>
+											<?php } ?>
+										<?php } ?>
+									<?php } else {?>
+										<td>
+											<?php echo '<input type="checkbox" name="albums[]" value="'.$albums[$i]->id.'" checked>';?>
+										</td>
+									<?php } ?> <!-- } else { -->
+							<? }?> <!-- for( ; sizeOf($albums); )-->
+						</table>
+					<? } ?> <!-- if(owner) -->
+				</div>
+			</div>
+  		</div>
   	  	<div class="panel-footer">
 	  		<div class="form-group">
 	  			<label for="text">Tags(Eralda komaga, ilma tühikuta)</label>
